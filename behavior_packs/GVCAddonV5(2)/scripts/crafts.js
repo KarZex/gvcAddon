df5cb0484577908e7b93286925f894130f4c78f9
KuromiChan <soel9e9lsub2@gmail.com>
2024-08-18 14:39:55 +0900

    ttyyhh

--- behavior_packs/GVCAddonV5(2)/scripts/crafts.js
+++ behavior_packs/GVCAddonV5(2)/scripts/crafts.js
@@ -6,8 +6,8 @@ export const craftData = {
       { "give":"zex:556m","texture":"textures/items/556m", "count":32, "cost":[ 2, 2, 0 ] },
       { "give":"zex:762m","texture":"textures/items/762m", "count":16, "cost":[ 2, 2, 0 ] },
       { "give":"zex:12m","texture":"textures/items/12m", "count":32, "cost":[ 2, 2, 0 ] },
-      { "give":"zex:40m","texture":"textures/items/40m", "count":16, "cost":[ 8, 2, 0 ] },
-      { "give":"zex:rocketm","texture":"textures/items/rocketm", "count":16, "cost":[ 4, 2, 0 ] },
+      { "give":"zex:40m","texture":"textures/items/40m", "count":16, "cost":[ 2, 4, 0 ] },
+      { "give":"zex:rocketm","texture":"textures/items/rocketm", "count":16, "cost":[ 4, 4, 0 ] },
       { "give":"gun:mgg","texture":"textures/items/mgg", "count":1, "cost":[ 1, 0, 0 ] },
       { "give":"gun:tank","texture":"textures/items/tank", "count":1, "cost":[ 1, 0, 0 ] }
     ]
